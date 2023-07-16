04fd2f16c6fc9175b4b606c1b3c384538062c7fe
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-07-16 19:58:01 +0200

    Add project files.

---
+++ number guessing/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace numberGuessing // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the Number! type a number and press enter!");
+            int guessedNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
