0b79e0edd5c20348d4afebbf12b8f0d260342bd5
BenDev0803 <96314707+BenDev0803@users.noreply.github.com>
2023-07-18 16:57:34 +0200

    made input from user and implemented if else statements

--- number guessing/Program.cs
+++ number guessing/Program.cs
@@ -7,8 +7,25 @@ namespace numberGuessing // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the Number! type a number and press enter!");
+            Console.WriteLine("Guess the Number! type a number from 1 to 10 and press enter!");
             int guessedNumber = Convert.ToInt32(Console.ReadLine());
+            Random computerChoice = new Random();
+            int computerNumber = computerChoice.Next(0,10);
+
+            if(guessedNumber == computerNumber)
+            {
+                Console.WriteLine(guessedNumber + " your number");
+                Console.WriteLine(computerNumber+ " computer number");
+                Console.WriteLine("congratulations! the number you guessed is right!");
+
+            }
+            else
+            {
+                Console.WriteLine(guessedNumber + " your number");
+                Console.WriteLine(computerNumber + " computer number");
+                Console.WriteLine("the number is wrong!");
+            }
+
         }
     }
 }
